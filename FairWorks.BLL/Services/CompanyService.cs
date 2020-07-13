277abaa54d1b6cee59de9735e1379f6f9bab46c5
OguzhanYuksel <yukseloguzhan4@gmail.com>
2020-07-13 20:03:09 +0300

    delete usings

--- FairWorks.BLL/Services/CompanyService.cs
+++ FairWorks.BLL/Services/CompanyService.cs
@@ -1,13 +1,7 @@
 ﻿using FairWorks.Core.Services;
-using FairWorks.DAL.Data.Repositories;
 using FairWorks.DAL.Data.UnitOfWork;
 using FairWorks.DAL.Entities;
 using FairWorks.DTO;
-using FairWorks.Mapping.ConfigProfile;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace FairWorks.BLL.Services
 {
